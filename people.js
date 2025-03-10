ab0e60e50a8a5186bc6b267e8f610d4d5f962120
RosyPetrella <petrellamr@gmail.com>
2025-03-10 14:07:33 +0100

    add function mergeObj and console.log

--- people.js
+++ people.js
@@ -3,3 +3,18 @@
 // Crea una funzione che non ha parametri.
 // La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies.
 // All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto. **
+
+const completeName = require("./names.js");
+const hobbies = require("./hobbies.js");
+
+function mergeObj() {
+  const { firstName, lastName } = completeName("Rosy", "Petrella");
+  const [hobbyOne, hobbyTwo, hobbyThree] = hobbies("PS", "Dnd", "Books");
+
+  return {
+    fullName: { firstName, lastName },
+    hobbies: [hobbyOne, hobbyTwo, hobbyThree],
+  };
+}
+
+console.log(mergeObj());
