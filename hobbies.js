39aa98875f7dbf5acb0a885713ffb3be555afe0f
RosyPetrella <petrellamr@gmail.com>
2025-03-10 13:35:54 +0100

    add function and export

--- hobbies.js
+++ hobbies.js
@@ -1,3 +1,9 @@
 // Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree.
 // La funzione dovrebbe restituire un array di stringhe contenenti gli hobby.
 // Esporta la funzione dal file.
+
+function hobbies(hobbyOne, hobbyTwo, hobbyThree) {
+  return [hobbyOne, hobbyTwo, hobbyThree];
+}
+
+export default hobbies;
